68af0d599a4c56b1b8235dc885e6e4c0f1b4395e
Zachary Gichiri <gichirigichuru@gmail.com>
2022-01-18 18:08:59 +0300

    add akan names valu to the functions

--- js/script.js
+++ js/script.js
@@ -10,3 +10,24 @@ function date(){
     gender = "female"
   }
 }
+   
+var centuary = parseInt(year.toString().substring(0, 2)) + 1
+var yearr = parseInt(year.toString().substring(2, 4))
+var dayOfWeek = (((centuary/4)-2*centuary-1) + ((5*year/4)) + ((26*(month+1)/10) + day))% 7
+
+if(centuary == 21){
+  dayOfWeek = Math.ceil(dayOfWeek)
+}else{
+  dayOfWeek = Math.round(dayOfWeek)
+} 
+
+
+let femaleAkanNames =[
+  "akosua", "adwoa", "abenaa", "akua", "yaa", "afua", "ama",  
+  ]
+  
+  let maleAkanNames =[
+  "kwasi", "kwadko", "kwabena", "kwaku", "yaw", "kofi", "kwame",
+  ]
+  
+  
