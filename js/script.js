8a4901be636c68a89ae6765d12db5d9efabf2e94
Zachary Gichiri <gichirigichuru@gmail.com>
2022-01-18 17:52:28 +0300

    changed birthday data collected to integer using parseInt

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function date(){
+  let day = parseInt(document.getElementById("day-of-birth").value);
+  let month = parseInt(document.getElementById("month-of-birth").value);
+  let year = parseInt(document.getElementById("year-of-birth").value);
+  let gender;
