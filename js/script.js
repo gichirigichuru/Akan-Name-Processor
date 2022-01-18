d4094cc35e90b6b6b662a8e76a67bed85dd2a584
Zachary Gichiri <gichirigichuru@gmail.com>
2022-01-18 18:06:10 +0300

    use of if statement

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ function date(){
   let month = parseInt(document.getElementById("month-of-birth").value);
   let year = parseInt(document.getElementById("year-of-birth").value);
   let gender;
+
+  if(document.getElementById("male").checked == true){
+    gender = "male"
+  }else if(document.getElementById("female").checked == true){
+    gender = "female"
+  }
+}
